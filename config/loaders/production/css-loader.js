8a46fe0caa6c0a4fd5f846009a6e8103514a6d7e
Bogdan Olteanu <zenchrondynamics@gmail.com>
2019-10-15 17:47:02 +0000

    Modified entry directive

--- config/loaders/production/css-loader.js
+++ config/loaders/production/css-loader.js
@@ -1,21 +1,30 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
 const cssLoader = {
-    test: /\.css$/,
-    use: [
-        {
-            // creates style nodes from JS strings
-            loader: "style-loader",
-            options: {
-                sourceMap: true
-            }
-        },
-        {
-            loader: 'css-loader',
-            options: {
-                importLoaders: 1,
-                sourceMap: true
-            }
-        },
-    ]
+  test: /\.css$/,
+  use: [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        publicPath: path.join(__dirname, 'dist')
+      }
+    },
+    {
+      // creates style nodes from JS strings
+      loader: 'style-loader',
+      options: {
+        sourceMap: true
+      }
+    },
+    {
+      loader: 'css-loader',
+      options: {
+        importLoaders: 1,
+        sourceMap: true
+      }
+    }
+  ]
 };
 
 module.exports = cssLoader;
--- config/loaders/production/sass-loader.js
+++ config/loaders/production/sass-loader.js
@@ -1,30 +1,31 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
 
 const sassLoader = {
-    test: /\.scss$/,
-    use: [
-        {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-                publicPath: '../../dist',
-            }
-        },
-        // Translates CSS into CommonJS
-        {
-            loader: 'css-loader',
-            options: {
-                sourceMap: false,
-            }
-        },
-        // Compiles Sass to CSS
-        {
-            loader: 'sass-loader',
-            options: {
-                // Prefer `dart-sass`
-                implementation: require('sass'),
-                sourceMap: false,
-            },
-        }
-    ]
+  test: /\.scss$/,
+  use: [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        publicPath: path.join(__dirname, 'dist')
+      }
+    },
+    // Translates CSS into CommonJS
+    {
+      loader: 'css-loader',
+      options: {
+        sourceMap: false
+      }
+    },
+    // Compiles Sass to CSS
+    {
+      loader: 'sass-loader',
+      options: {
+        // Prefer `dart-sass`
+        implementation: require('sass'),
+        sourceMap: false
+      }
+    }
+  ]
 };
 module.exports = sassLoader;
