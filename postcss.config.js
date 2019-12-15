e5783898bdea870d34153177fe7a3b9fb79a6e2f
Bogdan Olteanu <zenchrondynamics@gmail.com>
2019-12-15 08:16:23 +0000

    Added post-css-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-    map: false,
-    plugins: {
-        'postcss-placehold': {},
-        'postcss-cssnext': {},
-        'autoprefixer': {},
-        'postcss-browser-reporter': {},
-        'postcss-reporter': {}
-    }
+  map: false,
+  plugins: {
+    'postcss-import': {},
+    'postcss-placehold': {},
+    'postcss-preset-env': {
+      browsers: 'last 2 versions',
+    },
+    'autoprefixer': {},
+    'postcss-browser-reporter': {},
+    'postcss-reporter': {},
+  },
 };
